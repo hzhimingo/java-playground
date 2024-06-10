a3dc5036040f95def04aa4f89f6deb5888367d5e
hzhimingx <huangzhimingme@foxmail.com>
2024-06-10 16:32:35 +0800

    feat: 分布式锁

--- distribute-lock-stock/src/main/java/com/hzhimingo/playground/DistributeLockStockApplication.java
+++ distribute-lock-stock/src/main/java/com/hzhimingo/playground/DistributeLockStockApplication.java
@@ -1,9 +1,12 @@
 package com.hzhimingo.playground;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@MapperScan("com.hzhimingo.playground.dao")
 public class DistributeLockStockApplication {
 
     public static void main(String[] args) {
